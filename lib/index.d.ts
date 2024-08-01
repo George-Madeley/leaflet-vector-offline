77106d9842e32ff65663dba745f73767948d00e5
George Madeley <george.madeley@outlook.com>
2024-08-01 10:38:06 +0100

    Reconfigured default exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,2 @@
-import { vectorOfflineLayer, VectorOfflineLayer } from "./VectorOfflineLayer";
+export { vectorOfflineLayer, VectorOfflineLayer } from "./VectorOfflineLayer";
 export type { Status, DoneCallback, KeyedHtmlCanvasElement, LeafletLayerOptions, } from "./types";
-export { VectorOfflineLayer };
-export default vectorOfflineLayer;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { vectorOfflineLayer, VectorOfflineLayer } from "./VectorOfflineLayer";
+export { vectorOfflineLayer, VectorOfflineLayer } from "./VectorOfflineLayer";
 export type {
   Status,
   DoneCallback,
   KeyedHtmlCanvasElement,
   LeafletLayerOptions,
 } from "./types";
-export { VectorOfflineLayer };
-
-export default vectorOfflineLayer;
